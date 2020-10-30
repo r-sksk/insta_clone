adad615c1fdcb815dd93cac8c6b62f39298209b1
DESKTOP-DRAMV4V\Ryo <r.sksk818@gmail.com>
2020-10-30 13:51:02 +0900

    finish

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  attachment :profile_image
+  has_many :recipes, dependent: :destroy
+
+  validates :username, presence: true
+
+  def self.search(search)
+    if search
+      where(['username LIKE ?',"%#{search}%"])
+    else
+      all
+    end
+  end
+
+end
+
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,13 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+
+
+Refile.secret_key = 'af9c30027d22e964819d64f60601e027c0b1b11f6eb621348271b6593b6d010643814e5e425071f0aabd0460f85f98cab8f56174e6f7493085cc5ef7fb1b5e9f'
